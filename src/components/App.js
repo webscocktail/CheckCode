1e8acb257c8dd715a445d07d39b90a479081eb25
Sage <myworkidhere@gmail.com>
2021-03-06 19:59:02 +0530

    deploy my react app to github pages

---
+++ src/components/App.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect }  from 'react';
+import Editor from'./Editor'
+import useLocalStorage from '../hooks/useLocalStorage'
+import pic from './cclogoo.png'
+
+function App() {
+  const[html, setHtml] = useLocalStorage('html', '')
+  const[css, setCss] = useLocalStorage('css', '')
+  const[js, setJs] = useLocalStorage('js', '')
+  const[srcDoc, setSrcDoc]= useState('')
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+       setSrcDoc(`
+      <html>
+        <body>${html}</body>
+        <style>${css}</style>
+        <script>${js}</script>
+      </html>
+      `)
+    }, 250)
+
+    return() => clearTimeout(timeout)
+   }, [html, css, js])
+  
+
+
+  return (
+    <>
+    <div class="logo">
+      <img src={pic} alt="logo" className="pic"
+      height={70}
+      />
+    </div>
+    <div className="pane top-pane">
+      <Editor 
+      language="xml"
+      displayName="HTML"
+      value={html}
+      onChange={setHtml}
+      />
+      <Editor
+      language="xml"
+      displayName="CSS"
+      value={css}
+      onChange={setCss}
+      />
+      <Editor
+      language="xml"
+      displayName="JS"
+      value={js}
+      onChange={setJs}
+      />
+    </div>
+    <div className="pane">
+      <iframe
+      srcDoc={srcDoc}
+      title="output"
+      sandbox="allow-scripts"
+      frameBorder="0"
+      width="50%"
+      height="100%"
+       />
+    </div>
+    </>
+  )
+}
+
+export default App;
